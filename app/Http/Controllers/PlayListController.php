0a67d4930ff587419cf48b9d990ac663a6b1c699
priana13 <herian.sap@gmail.com>
2023-05-11 21:39:05 +0700

    first commit

---
+++ app/Http/Controllers/PlayListController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlayList;
+use Illuminate\Http\Request;
+
+class PlayListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PlayList $playList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PlayList $playList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PlayList $playList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PlayList $playList)
+    {
+        //
+    }
+}
---
+++ database/factories/MusicFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Random;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Music>
+ */
+class MusicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'title' => $this->faker->sentence(2),
+            'detail' => $this->faker->sentence(6), 
+            'url' => $this->faker->url(),
+            'thumbnail' => $this->faker->imageUrl(),
+            'audio' => $this->faker->url(),
+            'creator_id' => rand(1,4),
+            'is_premium' => false,
+
+        ];
+    }
+}
